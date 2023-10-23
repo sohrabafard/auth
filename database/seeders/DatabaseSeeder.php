d806a9cea0965d8b5c063b124eab1ac0e3f6e1cf
arshia <arshiayazdian@gmail.com>
2023-10-23 22:45:05 +0330

    Seeder for test Permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        // Create Roles
+        $admin = Role::create(['name' => 'admin']);
+        $user = Role::create(['name' => 'user']);
+
+// Create Permissions
+        $read = Permission::create(['name' => 'read']);
+        $write = Permission::create(['name' => 'write']);
+
+// Assign Permissions to Roles
+        $admin->givePermissionTo($read);
+        $admin->givePermissionTo($write);
+        $user->givePermissionTo($read);
     }
 }

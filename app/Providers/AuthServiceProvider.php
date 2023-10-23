32133af90d89214e0c7d6477190f5de077f8bf88
arshia <arshiayazdian@gmail.com>
2023-10-23 19:16:27 +0330

    Edit AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Carbon;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+        Passport::ignoreRoutes();
+        Passport::personalAccessTokensExpireIn(Carbon::now()->addHours(24));
     }
 }

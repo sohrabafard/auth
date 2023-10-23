b60ccec3229577af38d2fe88c9b4a88a99b957dd
arshia <arshiayazdian@gmail.com>
2023-10-23 22:49:45 +0330

    api routes

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,13 @@
 
 use App\Http\Controllers\Api\v1\AuthController;
 use Illuminate\Support\Facades\Route;
-
+use Laravel\Passport\Http\Middleware\CheckScopes;
 
 Route::group(['prefix' => 'v1'], function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::middleware('auth:api')->get('/logout', [AuthController::class, 'logout']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::get('logout', [AuthController::class, 'logout']);
 });
+
+Route::middleware(['auth:api', 'scope:read-data'])->get('/read', 'DataController@read');
+Route::middleware(['auth:api', 'role:admin', 'scope:write-data'])->get('/write', 'DataController@write');

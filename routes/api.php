23542810292c77ef9845b05adf4e558b991793cf
arshia <arshiayazdian@gmail.com>
2023-10-23 19:14:08 +0330

    add route

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\v1\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::middleware('auth:api')->get('/logout', [AuthController::class, 'logout']);
+});
